082aef8a06ff94da33976cd0ac3c1a7e1b97454d
David Passaro <passarod@os1.engr.oregonstate.edu>
2020-01-21 12:50:45 -0800

    finished matrix mult

--- test2.sh
+++ test2.sh
@@ -1,33 +1,14 @@
     #!/bin/bash
-    #tmp="tmpfile$$"
-    #touch $tmp
-    #matrix transpose $2 > $tmp
-    #cat $tmp
- #while read line1 <&3 && read line2 <&4
- #   do
-        #while read -a  word1 <line1 && read -a word1 <line1
- #        
- #      # printf -- "%s" "$line1"
-        #printf -- "%s""$line2"
- #       for i in $line1
- #       do
- #           echo ""
- #       done
-        
- #   done 3<$1 4<$tmp
- #   echo "next"
+
     
     tmp="tmpColFile$$"
     touch $tmp #named with PID
-    tmp2="tmpFile2$$" #named with PID
-    touch $tmp2
+
     final="FinalFile$$"
     touch $final #named with PID
-    matrix dim $1 > $final #get the size with dim --count = rows lineNum = col
+    matrix dim $1 > $final #get the size with dim --   r1= rows c1  = col of first matrix
     counter=0
-
-
-   # cat $tmpCol$$
+    #grab the dimentions of the 2 matricies
     while read line; do
         for word in $line; do
             if [ $counter -eq 0 ]
@@ -41,7 +22,7 @@
             ((counter++))
         done
     done <  $final
-    matrix dim $2 > $final #get the size with dim --count = rows lineNum = col
+    matrix dim $2 > $final #get the size with dim --  r2 = rows c2 = col of second matrix input
     counter=0
     while read line; do
         for word in $line; do
@@ -56,16 +37,7 @@
             ((counter++))
         done
     done < $final
-    echo "$r1 $c1 $r2 $c2"
-
-
-
-
-
-
-
-    #printf "h" > $final
-    #cat $final
+    printf "" > $final
     i=1
     sum=0
     while [ $i -le $r1 ]
@@ -78,52 +50,25 @@
             do  
                 cat $1 | head -n $i | tail -n 1 | cut -f $k > $tmp  #throw specified line in tmpCol file y
                 a=$(<$tmp)
-                #echo "a=$a"
                 cat $2 | head -n $k | tail -n 1 | cut -f $j > $tmp  #throw specified line in tmpCol file y
                 b=$(<$tmp)
-                #echo "b=$b"
                 sum=$(( $sum+($a * $b) ))
-                #echo "sum=$sum a=$a b=$b"
                 ((k++))
             done
-            #if [ $j -eq $c2 ]
-            #then
-            #    printf -- "%s\t" "$sum" >> $final 
-            #else 
-            #    printf -- "%s\n" "$sum" >> $final 
-            #fi
-            echo "sum=$sum"
+
+            if [ $j -eq $c2 ]
+            then
+                printf -- "%s\n" "$sum" >> $final 
+            else 
+                printf -- "%s\t" "$sum" >> $final 
+            fi
             sum=0
             ((j++))
         done
-        #echo "i= $i"
         ((i++))
     done
     
-   # cat  $final  #test tabs newlines
-    echo ""
-    rm -f $tmp2 #get rid of tempfile
+    cat -A $final  #test tabs newlines
     rm -f $tmp #get rid of tempfile
     rm -f $final #get rid of tempfile
 
-#rm -f $tmp
-#            while read -a  word1 <&5 && read -a word1 <&6
- #       do 
-  #          echo "$word1"
-   #         echo "$word2"
-    #    done 5<$tmpCol$$ 6<$fnlCol$$
-
-
-    #while [ $l -le $count ] #for each number in 1 line (should be same)
-      #  do 
-           # cut -f $l  $tmpCol$$ > #cut fields of cols one by one put in tmpCol
-        #cat $tmpCol$$
-         #   l=`expr $l + 1`
-
-       # done
-
-        #cat $tmpCol$$
-        
-                #echo "$value"
-                #cut -f $i $1 > $tmpCol$$ #cut fields of cols one by one put in tmpCol
-                #echo "$x"
